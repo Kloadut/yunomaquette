582b6f8cf650186849ef814bf6daa9182fa73f4c
Kload <kload@kload.fr>
2013-03-07 20:19:40 +0100

    Responsive stuffs

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 20) {
-            $("#top-bar-container").css('border-bottom', '3px solid #222');
-        } else {
-            $("#top-bar-container").css('border-bottom', 'none');
+        if ($(window).width() > 1024) {
+            if ($(window).scrollTop() > 20) {
+                $("#top-bar-container").css('border-bottom', '3px solid #222');
+            } else {
+                $("#top-bar-container").css('border-bottom', 'none');
+            }
         }
     });
 });
