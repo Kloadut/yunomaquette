e03a1212a519140fc82441420b4006a96dde3a52
Kload <kload@kload.fr>
2013-03-08 11:27:42 +0100

    Fonts + clean

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
-    $(window).on('scroll', function() {
+    $(window).scroll(function() {
         if ($(window).width() > 1024) {
             if ($(window).scrollTop() > 20) {
-                $("#top-bar-container").css('border-bottom', '3px solid #222');
+                if ($("#top-bar-container").css('border-bottom') != '3px solid #222') {
+                    $("#top-bar-container").css('border-bottom', '3px solid #222');
+                }
             } else {
+                if ($("#top-bar-container").css('border-bottom') != 'none') {
+                    $("#top-bar-container").css('border-bottom', 'none');
+                }
+            }
+        }
+    });
+    $(window).resize(function() {
+        if ($(window).width() < 1024) {
+            if ($("#top-bar-container").css('border-bottom') != '3px solid #222') {
+                $("#top-bar-container").css('border-bottom', '3px solid #222');
+            }
+        } else {
+            if ($("#top-bar-container").css('border-bottom') != 'none') {
                 $("#top-bar-container").css('border-bottom', 'none');
             }
         }
